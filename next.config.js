5ac5b07a205838b3aae593f8c039024fdd3506a2
ultimateitech14 <ultimateitech14@gmail.com>
2025-11-25 17:20:45 +0530

    Fix Netlify build config for Next.js middleware

--- next.config.js
+++ next.config.js
@@ -1,10 +1,2 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true, // kyunki tum src/app use kar rahe ho
-  },
-  // IMPORTANT: yahan 'output: "export"' BILKUL NA RAKHO
-};
-
+const nextConfig = { reactStrictMode: true };
 module.exports = nextConfig;
